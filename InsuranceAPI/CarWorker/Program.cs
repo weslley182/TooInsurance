924d6df2d99d12c8c37773e61f4625659e36a8b6
Wesley Machado <weslley182@gmail.com>
2023-06-04 16:04:57 -0300

    finish

---
+++ InsuranceAPI/CarWorker/Program.cs
@@ -0,0 +1,23 @@
+using CarWorker;
+using DataBaseModel.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using System.Reflection;
+
+var libraryConfig = new ConfigurationBuilder()
+        .SetBasePath(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location))
+        .AddJsonFile("appsettings.json")
+        .Build();
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+        services.AddDbContext<AppDbContext>(options =>
+        {            
+            options.UseSqlite(libraryConfig.GetConnectionString("InsuranceAPIConnectionRelativePath"));
+        });
+    })
+    .Build();
+
+await host.RunAsync();
